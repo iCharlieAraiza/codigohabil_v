58cbae9ed3343d3b092c01e542821d469bd8534e
iCharlieAraiza <eliptikman1234@gmail.com>
2022-01-27 00:13:37 -0600

    Add category querys

--- pages/paths/[slug].js
+++ pages/paths/[slug].js
@@ -2,15 +2,33 @@ import React from 'react';
 import { GET_POST_BY_SLUG, GET_ALL_CATEGORIES, GET_CATEGORY_BY_SLUG } from '../../lib/wordpress/api';
 import fetcher from '../../lib/fetcher';
 
-const categorie = (props) => {
+const category = (props) => {
   console.log("slug - id" );
   console.log(props);
-  return <div>
-    <h1>Categories</h1>
-  </div>;
+  const posts = props.params.data.category.posts.nodes;
+  const subCategories = props.params.data.category.children.nodes;
+  return ( <div>
+            <h1>Categories</h1>  
+            <h2>subCategories</h2>
+            <ul>
+              {subCategories.map(subCategory => {
+                return (
+                  <li key={subCategory.id}>
+                    {subCategory.name}
+                  </li>
+                )
+              })}
+
+            </ul>
+
+            <h2>Posts</h2>
+            <ul>
+              {posts.map(post => <li key={post.id}>{post.title}</li>)}+
+            </ul>
+          </div>)
 };
 
-export default categorie;
+export default category;
 
 export async function getStaticPaths() {
     const response = await fetcher(GET_ALL_CATEGORIES);
