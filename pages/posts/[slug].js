7acc57f9b1932dbbc7cc5961419dfab035eec29f
iCharlieAraiza <eliptikman1234@gmail.com>
2022-02-04 17:29:19 -0600

    Add new dates

--- pages/posts/[slug].js
+++ pages/posts/[slug].js
@@ -8,7 +8,7 @@ import Head from 'next/head';
 const post = ({ postData }) => {
 
   const post = postData.data.post;
-  console.log(post.seo.fullHead);  
+  //console.log(post.seo.fullHead);  
 
   return (
     <>
