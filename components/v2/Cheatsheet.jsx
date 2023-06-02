6b830e449dc23796517a5fc179fca41b2549c79c
iCharlieAraiza <eliptikman1234@gmail.com>
2023-06-01 18:31:17 -0600

    Remove dummy data

--- components/v2/Cheatsheet.jsx
+++ components/v2/Cheatsheet.jsx
@@ -19,61 +19,8 @@ const Cheatsheet = ({post}) => {
         <h1>📝 {post?.title}</h1>
 
         <article dangerouslySetInnerHTML={{__html: post?.content}}>
-
         </article>
-        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Etiam varius dolor quam, sit amet suscipit nisi egestas eget. Aliquam at purus a ante pellentesque eleifend. Sed eu tortor at lacus eleifend commodo. Donec id justo mattis, tincidunt lectus vitae, ultrices.</p>
 
-        <h2>Modificar arrays</h2>
-        <section className="three-section">
-          <div className="cheatsheet-block">
-            <h3 className="cheatsheet-header">Map</h3>
-            <div className="content-block">
-              <p>
-                <b>Valor que regresa:</b> array
-              </p>
-              <p>
-                Esta función combina dos arrays y lo regresa como un nuevo
-                array.
-              </p>
-            </div>
-          </div>
-          <div className="cheatsheet-block">
-            <h3 className="cheatsheet-header">Map</h3>
-            <div className="content-block">
-              <p>
-                <b>Valor que regresa:</b> array
-              </p>
-              <p>
-                Esta función combina dos arrays y lo regresa como un nuevo
-                array.
-              </p>
-            </div>
-          </div>
-          <div className="cheatsheet-block">
-            <h3 className="cheatsheet-header">Map</h3>
-            <div className="content-block">
-              <p>
-                <b>Valor que regresa:</b> array
-              </p>
-              <p>
-                Esta función combina dos arrays y lo regresa como un nuevo
-                array.
-              </p>
-            </div>
-          </div>
-          <div className="cheatsheet-block">
-            <h3 className="cheatsheet-header">Map</h3>
-            <div className="content-block">
-              <p>
-                <b>Valor que regresa:</b> array
-              </p>
-              <p>
-                Esta función combina dos arrays y lo regresa como un nuevo
-                array.
-              </p>
-            </div>
-          </div>
-        </section>
       </CheatsheetComponent>
     </div>
   );
