9c9bbe683a18dab8d8b282d6c79782af57513526
iCharlieAraiza <eliptikman1234@gmail.com>
2023-09-23 01:11:29 -0600

    Remove unused dependencies

--- pages/index.js
+++ pages/index.js
@@ -1,14 +1,6 @@
 import Head from 'next/head'
-import Image from 'next/image'
-import styles from '../styles/Home.module.css'
-import Header from '../components/Header'
-import Banner from '../components/Banner'
-import MainHomepage from '../components/MainHomepage'
-//import { Topics, Navbar, Hero, About } from '../components/V2'
-
 import { Navbar, Hero, About, Topics} from '../components/v2'
 
- 
 import { GET_ALL_CATEGORIES } from '../lib/wordpress/api'
 import fetcher from '../lib/fetcher'
 import Script from 'next/script'
@@ -46,8 +38,6 @@ export default function Home({ categories, theme}) {
     </div>
   )
 }
-
-
 
 export async function getStaticProps() {
   const categories = await fetcher(GET_ALL_CATEGORIES)
